2efab3bbf5a448bccd501c56db94b186686c8b39
C. Mark Halberstadt <chalbers2@gmail.com>
2014-12-23 18:06:25 -0700

    Initial Commit
    First commit of the repository - first version of the website

---
+++ js/indexJS.js
@@ -0,0 +1,32 @@
+// this is where the javascript begins
+
+var numFadeSeconds = 0;
+
+
+
+
+//$(document).load(function() {
+    //$('img').hide();
+//});
+
+/*
+
+$(document).ready(function() {
+    $('img').hide();
+    $('#cMarkHalberstadtLabel_MainPage').fadeIn(numFadeSeconds / 6 * 1000);
+    $('#engineerLabelImg').fadeIn(numFadeSeconds / 3 * 1000);
+    $('#dash_1_Img').fadeIn(numFadeSeconds / 2 * 1000);
+    $('#musicianLabelImg').fadeIn(numFadeSeconds / 3 * 2 * 1000);
+    $('#dash_2_Img').fadeIn(numFadeSeconds / 6 * 5 * 1000);
+    $('#teacherLabelImg').fadeIn(numFadeSeconds * 1000);
+    $('#cMarkHalberstadtLabel_MainPage').hide().fadeIn(1000);
+});
+ 
+ */
+ 
+
+ 
+ 
+
+
+
---
+++ js/teacherJS.js
@@ -0,0 +1,5 @@
+// this is where the javascript begins
+
+
+
+
