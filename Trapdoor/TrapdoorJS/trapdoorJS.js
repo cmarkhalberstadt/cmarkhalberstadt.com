d01ec8ec03978c363761f7bebc3de2b455fcf80c
C. Mark Halberstadt <chalbers2@gmail.com>
2015-01-02 09:01:04 -0700

    Files added

---
+++ Trapdoor/TrapdoorJS/trapdoorJS.js
@@ -0,0 +1,10 @@
+// this is where the javascript begins
+
+var x = 7;
+var y = 12;
+
+console.log("Answer is:");
+console.log(x+y);
+
+
+
--- js/indexJS.js
+++ js/indexJS.js
@@ -1,28 +1,32 @@
 // this is where the javascript begins
 
-var numFadeSeconds = 0;
+var numFadeSeconds = 2.3;
 
-
-
-
-//$(document).load(function() {
-    //$('img').hide();
-//});
-
-/*
-
-$(document).ready(function() {
-    $('img').hide();
+window.onload = function() {
+    
     $('#cMarkHalberstadtLabel_MainPage').fadeIn(numFadeSeconds / 6 * 1000);
+    $('#cMarkHalberstadtLabel_MainPage').css("display", "block");
+
     $('#engineerLabelImg').fadeIn(numFadeSeconds / 3 * 1000);
+    $('#engineerLabelImg').css("display", "block");
+
     $('#dash_1_Img').fadeIn(numFadeSeconds / 2 * 1000);
+    $('#dash_1_Img').css("display", "block");
+
     $('#musicianLabelImg').fadeIn(numFadeSeconds / 3 * 2 * 1000);
+    $('#musicianLabelImg').css("display", "block");
+
     $('#dash_2_Img').fadeIn(numFadeSeconds / 6 * 5 * 1000);
+    $('#dash_2_Img').css("display", "block");
+
     $('#teacherLabelImg').fadeIn(numFadeSeconds * 1000);
-    $('#cMarkHalberstadtLabel_MainPage').hide().fadeIn(1000);
-});
+    $('#teacherLabelImg').css("display", "block");
+
+    $('#cMarkHalberstadtLabel_MainPage').fadeIn(1000);
+    $('#cMarkHalberstadtLabel_MainPage').css("display", "block");
+};
  
- */
+
  
 
  
