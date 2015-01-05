d492705415794866063c791d8f717572f2113b2d
C. Mark Halberstadt <chalbers2@gmail.com>
2015-01-05 09:57:58 -0700

    Added Files

---
+++ js/trapdoorPositionJS.js
@@ -0,0 +1,11 @@
+// this is where the javascript begins
+
+
+window.onload = function(){
+    var height = $(document).height();
+    var width  = $(document).width();
+    
+    $("#TrapdoorLink").css("top", height-80);
+    
+};
+
